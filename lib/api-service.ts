619cd2ca7909ca5f8c22deae0ff5a7ceb9e09fb8
salwawawa <salwaputri260@gmail.com>
2025-06-16 00:38:53 +0700

    update backend url

--- lib/api-service.ts
+++ lib/api-service.ts
@@ -1,6 +1,6 @@
 import { Shop, Product, Category, Block, HouseNumber, Activity } from './types';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://http://165.22.100.204:3001/';
 
 export class ApiService {
   private baseURL: string;
