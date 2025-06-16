67fd63a892ad8f184da58994124d1d9b066a22e6
salwawawa <salwaputri260@gmail.com>
2025-06-16 11:43:42 +0700

    update api

--- lib/api-service.ts
+++ lib/api-service.ts
@@ -1,6 +1,6 @@
 import { Shop, Product, Category, Block, HouseNumber, Activity } from './types';
 
-const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_URL || 'https://sentratamansari.com'}/api/v1`;
+const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_URL || 'https://sentratamansari.com'}`;
 
 export class ApiService {
   private baseURL: string;
