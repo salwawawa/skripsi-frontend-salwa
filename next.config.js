3b85b36c017e11a9174ce311fe16c3a1d25c0863
salwawawa <salwaputri260@gmail.com>
2025-06-16 21:46:32 +0700

    update orientasi logo & config url backend

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'], // Add your backend hostname here
+    domains: ['sentratamansari.com'], // Add your backend hostname here
   },
 };
 
